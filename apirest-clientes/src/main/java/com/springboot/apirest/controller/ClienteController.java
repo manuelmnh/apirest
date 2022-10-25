4c3b7968276e0b49bbb02d3f106cd1e67a49b168
117305 <117305@IBLAESMADC01300.iecisa.corp>
2022-10-25 12:12:52 +0200

    primer commit

--- apirest-clientes/src/main/java/com/springboot/apirest/controller/ClienteController.java
+++ apirest-clientes/src/main/java/com/springboot/apirest/controller/ClienteController.java
@@ -1,9 +1,14 @@
 package com.springboot.apirest.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.hibernate.query.criteria.internal.expression.ConcatExpression;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,7 +39,28 @@ public class ClienteController {
 		
 	}
 	
-	@SuppressWarnings("unlikely-arg-type")
+	@GetMapping("/clientes/{id}")
+	public ResponseEntity<?> show(@PathVariable Long id){
+		Cliente cliente = null;
+		Map<String,Object> response = new HashMap<>();
+		
+		try {
+			cliente = clienteService.findClientById(id);
+		}catch (DataAccessException e){
+			response.put("mensaje", "Error al realizar la consulta a base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if(cliente == null) {
+			response.put("mensaje", "El cliente con ID: "+id.toString()+" no existe en base de datos");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Cliente>(cliente,HttpStatus.OK);
+	}
+	
+	/*@SuppressWarnings("unlikely-arg-type")
 	@GetMapping("/clientes/{id}")
 	public Cliente show(@PathVariable Long id) throws Exception {
 		Cliente cliente = new Cliente();
@@ -50,7 +77,7 @@ public class ClienteController {
 		
 		return cliente;
 		
-	}
+	}*/
 	
 	@PostMapping("/saveClient")
 	@ResponseStatus(HttpStatus.CREATED)
